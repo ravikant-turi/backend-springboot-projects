823aa0ec27edae7c7bdfc210caa5e814a3b8b4d1
Ravikant Turi <145963203+ravikant-turi@users.noreply.github.com>
2026-08-21 23:50:32 +0530

    QuestionController is completed with crud operation

--- QuizApp/src/main/java/com/java/quiz/controller/QuestionController.java
+++ QuizApp/src/main/java/com/java/quiz/controller/QuestionController.java
@@ -11,7 +11,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+/**
+ * QuestionController
+ */
 
 @RestController
 @RequestMapping("api/questions/")
