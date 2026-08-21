8770b06d07bc72ed4c0bd9cc89aea0bd12b6a8be
Ravikant Turi <145963203+ravikant-turi@users.noreply.github.com>
2026-08-21 23:33:23 +0530

    question controller is created

--- QuizApp/src/main/java/com/java/quiz/controller/QuestionController.java
+++ QuizApp/src/main/java/com/java/quiz/controller/QuestionController.java
@@ -1,24 +1,24 @@
-//package com.java.quiz.controller;
-//
-//import com.java.quiz.dto.*;
-//import com.java.quiz.model.Question;
-//import com.java.quiz.payloads.ApiResponse;
-//import com.java.quiz.service.QuestionService;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.validation.Valid;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("api/questions/")
-//public class QuestionController {
-//    @Autowired
-//    private QuestionService questionService;
-//
+package com.java.quiz.controller;
+
+import com.java.quiz.dto.*;
+import com.java.quiz.model.Question;
+import com.java.quiz.payloads.ApiResponse;
+import com.java.quiz.service.QuestionService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/questions/")
+public class QuestionController {
+    @Autowired
+    private QuestionService questionService;
+
 //    @GetMapping
 //    public ResponseEntity<ApiResponse<PagedResponse<GetQuestionResponseDto>>> findAllQuestion(
 //            HttpServletRequest httpServletRequest
@@ -74,4 +74,4 @@
 //    }
 //
 //
-//}
+}
