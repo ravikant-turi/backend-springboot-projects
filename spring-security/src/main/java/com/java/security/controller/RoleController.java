fe521e9333c3042cbc465287a45e34557cfe682e
Ravikant Turi <ravikantt@infinite.com>
2026-09-04 16:39:02 +0530

    Spring Security DB Authentication & Password Security | Spring Boot Full Course #36

---
+++ spring-security/src/main/java/com/java/security/controller/RoleController.java
@@ -0,0 +1,10 @@
+package com.java.security.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/role")
+public class RoleController {
+
+}
